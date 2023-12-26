eab15a0f032687156212942661786623495e07c0
AshutoshRaghubansi <ashutosh.raghubansi@pramericalife.in>
2023-12-26 11:10:31 +0000

    first

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = ' Node  Ashutosh  Aditya\n'
+  const msg = 'Hello  Node  Ashutosh  Aditya\n'
   res.end(msg);
 });
 
