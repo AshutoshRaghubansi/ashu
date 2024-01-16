964582fa82fb65cab0ccd1f0a16f069c624646e3
AshutoshRaghubansi <ashutosh.raghubansi@pramericalife.in>
2024-01-16 09:53:25 +0000

    as

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 30000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello PM2   Webpack Node React node1  Engineering .\n'
+  const msg = 'Hello PM2   Webpack Node React node1  Engineering Legends .\n'
   res.end(msg);
 });
 
